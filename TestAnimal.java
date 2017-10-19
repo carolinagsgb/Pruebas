c83541fcff8b337d1127332b6c6e157818ae43f1
carolinagsgb <32925557+carolinagsgb@users.noreply.github.com>
2017-10-19 10:58:01 +0200

    Añadir fichero java con UPLOAD

---
+++ TestAnimal.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class TestAnimal {
+	
+	public static void main (String [] args) {
+
+	animal animal1 = new animal ();
+
+	Scanner teclado = new Scanner (System.in);
+
+	System.out.print("Teclee el nombre del animal");
+
+	animal1.setNombre(teclado.nextLine());
+
+	System.out.print ("Teclee la edad del animal: ");
+	int edad = teclado.nextInt();
+
+	animal1.setEdad(edad);
+
+	animal1.nace();
+
+	System.out.println("Me llamo "+animal1.getNombre()+" y tengo "+ animal1.getEdad()+" años");
+	}
+}
